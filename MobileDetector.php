0ad6d64d35210d38e41bc733969ed5295d1fe012
Shude <babah@kekx.ru>
2017-01-21 03:27:18 +0300

    Version 1.0.0

--- MobileDetector.php
+++ MobileDetector.php
@@ -1,27 +1,62 @@
 <?php
-
-namespace shude\mobiledetector;
-
-use yii;
-use Detection\MobileDetect;
-use yii\base\Component;
-
 /**
- * DeviceDetect
+ * MobileDetector
  *
  * @author Vladimir Tarkhanov <shu-de@kekx.ru>
  * @version 1.0.0
  */
+namespace shude\mobiledetector;
 
+use Yii;
+use Detection\MobileDetect;
+use yii\base\Component;
+
+
+/**
+ * Class MobileDetector
+ *
+ * This component detects user device from User-Agent string and replace
+ * view paths if needed.
+ *
+ * Best choise to use the component is add it to a bootstrap section in configuration.
+ *
+ * @package shude\mobiledetector
+ */
 class MobileDetector extends Component {
 
-	private $_md;
-	public  $rewritePaths = false;
+    /**
+     * @var bool whether to replace view paths
+     */
+    public  $rewritePaths = false;
+
+    /**
+     * @var string if $rewritePaths is true and user device detected as 'mobile' then
+     * view path replaced on this value
+     */
 	public  $mobileViewPath  = '@app/views/mobile';
+
+    /**
+     * @var string if $rewritePaths is true and user device detected as 'desktop' then
+     * view path replaced on this value
+     */
     public  $desktopViewPath = '@app/views/desktop';
+
+    /**
+     * @var string if $rewritePaths is true and user device detected as 'tablet' then
+     * view path replaced on this value
+     */
     public  $tabletViewPath  = '@app/views/tablet';
 
+    /**
+     * @var string current device type
+     */
     private $_device = 'desktop';
+
+    /**
+     * MobileDetect library instance
+     * @var \Detection\MobileDetect
+     */
+    private $_md;
 
 	public function __construct($config = array()) {
 		parent::__construct($config);
@@ -50,9 +85,12 @@ class MobileDetector extends Component {
                 Yii::$app->setLayoutPath($this->desktopViewPath.'/layouts');
             }
         }
+
+        unset ($this->_md);
 	}
 
     /**
+     * Returns a string that represent a user device type.
      * @return string mobile|tablet|desktop
      */
     public function device(){
